ecb7ff3c71ecaacb434f73ddf09d575588833cb6
TrinhBac <Trinhbac1907@gmail.com>
2021-12-08 11:48:22 +0700

    update migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('username');
-            $table->string('address');
-            $table->string('phone');
+            $table->string('name')->nullable();
+            $table->string('username')->nullable();
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
